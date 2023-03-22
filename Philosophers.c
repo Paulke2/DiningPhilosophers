bb29594793886f8f686f7d493d3f062345f859da
Paulke2 <paulke@udel.edu>
2023-03-22 16:40:58 -0400

    modeled problem, now fix

--- Philosophers.c
+++ Philosophers.c
@@ -1,6 +1,60 @@
-#include<stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#define Num_philosophers 5
+#define Num_chopsticks 5
+sem_t chop_stick[5];
+//sem_t hungry;
+int hungry=0;
+typedef struct arg_data {
+	int thread_number;
+} arg_data;
+ 
+void* philosopher(void* arg)
+{
+	arg_data*current_thread_data = (arg_data*)arg;
+ while(hungry==0);
+    while (1) {
+ //wait left chop, 
+ 	sem_wait(&chop_stick[(current_thread_data->thread_number+1)%Num_philosophers]);
+	printf("philosopher %d has picked up chopstick number %d\n",current_thread_data->thread_number, ((current_thread_data->thread_number+1)%Num_philosophers));
+	
+	sem_wait(&chop_stick[current_thread_data->thread_number]);
+      	printf("philosopher %d has picked up chopstick number %d is ready to eat\n",current_thread_data->thread_number, current_thread_data->thread_number);
+	
+    }
+    return NULL;
+}
+ 
 int main()
 {
-    printf("Hello, World");
-    return 0;
+ 
+ //sem_init(&hungry,0,0); 
+ pthread_t id[Num_philosophers];
+ arg_data arg_arr[Num_philosophers];
+    for (int i = 0; i < Num_philosophers; i++) {
+ //init list of chopsticks
+    sem_init(&chop_stick[i],0,1);
+	arg_arr[i].thread_number = i;
+        // create philosopher processes
+        pthread_create(&id[i], NULL,
+                       philosopher, &arg_arr[i]);
+ 
+        printf("Philosopher %d is thinking\n", i);
+	if(i == Num_philosophers -1){
+		//this is so they all become hungry at the same time
+		hungry =1;
+	}
+    }
+ 
+    for (int i = 0; i < Num_philosophers; i++){
+ 
+        pthread_join(id[i], NULL);
+}
+return 0;
 }
